6647cf1a6c2b1bf031c397361346d4fcc8e969ed
febry40 <febryevayonisa@gmail.com>
2024-03-25 14:49:46 +0700

    input penjumlahan

--- 085_FungsiSwitch/085_FungsiSwitch.cpp
+++ 085_FungsiSwitch/085_FungsiSwitch.cpp
@@ -15,22 +15,34 @@ float penjumlahan(float a, float b)
 {
 	return a + b;
 }
+
 float pembagian(float a, float b)
 {
 	return a / b;
 }
+
 float perkalian(float a, float b)
 {
 	return a * b;
 }
+
 float pengurangan(float a, float b)
 {
 	return a - b;
 }
 
+int main()
+{
+	int pilihan;
 
-
-
+	do
+	{
+		system("CLS");
+		cout << "===========================================" << endl;
+		cout << "MENU" << endl;
+		cout << "1. Penjumlahan " << endl;
+	}
+}
 
 
 
